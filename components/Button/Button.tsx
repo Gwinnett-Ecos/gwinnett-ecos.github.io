1fcbb938af30fb70e6f9cd37c0a5f4ff44a9d5ce
Yubo-Cao <cao2006721@gmail.com>
2023-11-21 23:50:42 -0500

    feat: add Button component

---
+++ components/Button/Button.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+export type ButtonProps = {
+  children?: React.ReactNode;
+  text?: string;
+  variant?: "filled" | "secondary";
+  color: string;
+  className?: string;
+  props?: React.HTMLProps<HTMLButtonElement>;
+};
+
+export default function Button({
+  children,
+  text,
+  color,
+  variant = "filled",
+  className,
+  ...props
+}: ButtonProps) {
+  if (text && children) {
+    throw new Error(
+      `You cannot pass both text and children to the Button component.`
+    );
+  }
+  if (!text && !children) {
+    throw new Error(
+      `You must pass either text or children to the Button component.`
+    );
+  }
+
+  const buttonClass = `inline-flex justify-center items-center ${
+    variant === "filled" ? `bg-${color}-500 text-white` : "text-slate-600"
+  }`;
+
+  return (
+    <div
+      className={`px-4 py-2 ${buttonClass} ${className} rounded-full`}
+      {...props}
+    >
+      {text && (
+        <span className="font-normal text-xs leading-normal md:text-lg md:leading-7">
+          {text}
+        </span>
+      )}
+      {children}
+    </div>
+  );
+}
