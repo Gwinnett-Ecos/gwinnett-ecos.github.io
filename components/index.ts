707b3d449d1e17c072927b1ebdc26915e2fb226e
Yubo-Cao <cao2006721@gmail.com>
2023-11-21 23:50:34 -0500

    refact: import from components

--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,4 @@
-import Logo from "./Logo/Logo";
+export { default as Logo } from "./Logo/Logo";
+export type { LogoProps } from "./Logo/Logo";
+export { default as Button } from "./Button/Button";
+export type { ButtonProps } from "./Button/Button";
