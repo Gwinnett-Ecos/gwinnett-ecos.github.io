27513df6c9ca3c44edad8babadc37cbea73496e9
Yubo-Cao <cao2006721@gmail.com>
2023-11-21 23:35:03 -0500

    feat: add logo component

---
+++ components/Logo/Logo.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import image from "./logo.png";
+import Image from "next/image";
+
+export type LogoProps = {
+  className?: string;
+  variant: "logo-only" | "logo-and-text";
+  props?: React.HTMLProps<HTMLDivElement>;
+};
+
+const assertVariant = (variant: string): void => {
+  if (variant !== "logo-only" && variant !== "logo-and-text") {
+    throw new Error(
+      `Invalid variant '${variant}' passed to Logo component. Valid variants are 'logo-only' and 'logo-and-text'.`
+    );
+  }
+};
+
+export default function Logo({
+  className,
+  variant = "logo-only",
+  ...props
+}: LogoProps) {
+  assertVariant(variant);
+  const logo = (
+    <div
+      className={`flex items-center justify-center h-16 w-16 rounded-full bg-white ${
+        variant === "logo-only" && className
+      } overflow-clip bg-white`}
+    >
+      <Image
+        src={image}
+        alt="Gwinnett Ecos Logo"
+        width={53.87}
+        height={43.51}
+      />
+    </div>
+  );
+  if (variant === "logo-only") {
+    return logo;
+  }
+  const text = (
+    <div className="flex-col justify-center items-start inline-flex">
+      <div className="text-lg lg:text-xl font-light leading-tight text-slate-600">
+        Gwinnett
+      </div>
+      <div className="text-lg lg:text-xl font-black leading-tight text-primary-1-500">
+        Ecos
+      </div>
+    </div>
+  );
+  return (
+    <div
+      className="flex items-center justify-center gap-2.5 lg:gap-1.5"
+      {...props}
+    >
+      {logo}
+      {text}
+    </div>
+  );
+}
