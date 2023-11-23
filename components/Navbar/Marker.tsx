2909d245bd1c68cab99805d75b77ceda898f5fae
Yubo-Cao <cao2006721@gmail.com>
2023-11-22 23:35:19 -0500

    feat: add children prop to Marker component and fix nonjumpable problem

--- components/Navbar/Marker.tsx
+++ components/Navbar/Marker.tsx
@@ -2,10 +2,13 @@ export type MarkerProps = {
   title: string;
   icon: string;
   id: string;
+  children?: React.ReactNode;
 };
 
-export default function Marker({ title, id, icon }: MarkerProps) {
+export default function Marker({ title, id, icon, children }: MarkerProps) {
   return (
-    <a id={id} data-title={title} data-icon={icon} hidden aria-hidden="true" />
+    <a id={id} data-title={title} data-icon={icon}>
+      {children}
+    </a>
   );
 }
