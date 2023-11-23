149d1d83d228d508f66353eb20d8a401ffc72279
Yubo-Cao <cao2006721@gmail.com>
2023-11-22 23:32:37 -0500

    feat(components): Refactor Footer component to dynamically generate links

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,11 +1,31 @@
 "use client";
+import { useEffect, useRef, useState } from "react";
 import { Logo } from "..";
 import InstagramIcon from "./InstagramIcon";
+import Link from "next/link";
 
 export default function Footer() {
+  const [items, setItems] = useState<
+    {
+      title: string;
+      id: string;
+    }[]
+  >([]);
+  const selector = "a[id][data-title][data-icon]";
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setItems(
+        [...document.querySelectorAll(selector)].map((element) => ({
+          title: element.getAttribute("data-title")!!,
+          id: element.id,
+        }))
+      );
+    }
+  }, [setItems]);
+
   return (
     // TODO: fix footer under sm breakpoint
-    <div className="w-[100vw] md:relative -translate-x-[1.5rem] sm:w-full sm:translate-x-0 md:w-[100vw] md:-translate-x-[calc((100vw-100%+3rem)/2)] lg:-translate-x-[calc((100vw-1024px+3rem)/2)] px-6 pt-2 pb-8 md:pb-12 bg-primary-1-100 flex justify-center gap-2.5">
+    <div className="w-[100vw] md:relative -translate-x-[1.5rem] sm:w-full sm:translate-x-0 md:w-[100vw] md:-translate-x-[calc((100vw-100%+3rem)/2)] lg:-translate-x-[calc((100vw-1024px+3rem)/2)] px-6 pt-2 md:pt-8 pb-8 md:pb-16 bg-primary-1-100 flex justify-center gap-2.5">
       <div className="self-stretch justify-start items-start inline-flex w-full max-w-5xl">
         <div className="grow shrink flex-col justify-start items-start inline-flex md:gap-2">
           <div className="text-lime-700 text-xs md:text-base font-normal leading-normal">
@@ -32,7 +52,15 @@ export default function Footer() {
             </button>
           </div>
           <div className=" text-lime-700 text-xs md:text-base font-bold leading-normal">
-            About Us · Events · Team
+            {items.map((item, idx) => {
+              const { title, id } = item;
+              return (
+                <>
+                  <Link href={`#${id}`}>{title}</Link>
+                  {idx !== items.length - 1 && <span>{" · "}</span>}
+                </>
+              );
+            })}
           </div>
         </div>
         <Logo />
