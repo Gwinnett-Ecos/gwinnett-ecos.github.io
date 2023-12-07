fb41c2d6866538bd1500431a2486e21a605ba972
Yubo Cao <cao2006721@gmail.com>
2023-12-06 20:11:54 -0500

    fix: refactor next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
-
-module.exports = (phase) => {
-  /** @type {import('next').NextConfig} */
-  const config = {
-    reactStrictMode: true,
-    output: "export",
-  };
-
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    config.env = {
-      API_URL: "http://localhost:3000",
-    };
-    config.images = {
-      unoptimized: true,
-    };
-  }
-
-  return config;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
 };
+
+module.exports = nextConfig;
